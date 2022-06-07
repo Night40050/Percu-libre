9a43260bbd2ca6e40bdae09243c314854248e1a0
Night40050 <danieleduaedo13@gmail.com>
2022-06-07 10:27:08 -0500

    versión inicial

---
+++ main.js
@@ -0,0 +1,14 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase1');
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 500});
+ScrollReveal().reveal('.news-cards-two', {delay: 500});
+ScrollReveal().reveal('.cards-banner-two', {delay: 500});
+ScrollReveal().reveal('.social', {delay: 500});
+ScrollReveal().reveal('.comentario',{delay:500});
+ScrollReveal().reveal('.footer-links', {delay: 500});
+ScrollReveal().reveal('.footer', {delay: 500});
